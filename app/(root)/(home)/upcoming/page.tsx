2d9528629c8f3380c62161dffad1ff15e730fc60
Mohammad Khalid Hasan <rabbikhalidhasan@gmail.com>
2024-08-24 01:43:20 +0100

    HomeCard

---
+++ app/(root)/(home)/upcoming/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpComing = () => {
+  return (
+    <section className="flex flex-col size-full gap-10 text-white">
+      <h1 className="text-3xl font-bold">UpComing</h1>
+    </section>
+  );
+};
+
+export default UpComing;
